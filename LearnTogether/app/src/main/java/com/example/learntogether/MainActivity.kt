acbb8cdce52c7d21754feeb830eb3582f53d9600
@asanonymousig <ashrimate@gmail.com>
2025-05-21 22:23:14 +0530

    Initial Commit
    4 projects of Android Studio app devlopment are done uptill now:
    1. Greeting
    2. HappyBirthday
    3. LearnTogether
    4. TaskManager

---
+++ LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learntogether.ui.theme.LearnTogetherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearnTogetherTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    LearnTogether(
+                        heading = stringResource(R.string.heading),
+                        paragraph1 = stringResource(R.string.paragraph1),
+                        paragraph2 = stringResource(R.string.paragraph2)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LearnTogether(heading: String, paragraph1: String, paragraph2: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+        )
+        Text(
+            text = heading,
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(all = 16.dp)
+        )
+        Text(
+            text = paragraph1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = paragraph2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(all = 16.dp)
+        )
+    }
+
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun Preview() {
+//    LearnTogetherTheme {
+//        LearnTogether("Hello", "Hi", "Hola")
+//    }
+//}
